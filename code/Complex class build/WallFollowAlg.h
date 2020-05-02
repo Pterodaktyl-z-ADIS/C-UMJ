524beaf8a2d185753d5b9f1462aae77a449e19a0
Foreseeker <49638998+Foreseeker@users.noreply.github.com>
2020-05-02 21:03:05 +0200

    Wall follow algorythm
    Not completed yet

---
+++ code/Complex class build/WallFollowAlg.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "Labirynth.h"
+#include "Sensors.h"
+#include "Position.h"
+
+Labirynth labirynth;
+Sensors sensors;
+Position position;
+
+
+
+bool Algorythm(int X, int Y)
+{
+//ustawienie myszy w konkretnym punkcie
+	labirynth.setMouse[X][Y];
+
+//odœwie¿anie ekranu
+	labirynth.printMaze();
+	_sleep(50);
+
+//sprawdzania, czy zosta³o odnalezione wjœcie
+	if (X == labirynth.getDoor[0] && Y == labirynth.getDoor[1]) {
+		return true;
+	}
+
+//rekurencyjne poszukiwanie rozwi¹zania
+// sprawdzanie, czy jestem na mapie i nie stojê w przeszkodzie 
+	if (X != labirynth.getStart[0] && X < 1 || X > 18) {
+		return false;
+	}
+	if (Y != labirynth.getStart[1] && Y < 1 || Y > 18) {
+		return false;
+	}
+	if (labirynth.isObstacle[X][Y]) {
+		return false;
+	}
+
+// sprawdzanie wolnych pól wokó³
+	if (position.getOrientation == 'N') {
+		if (!labirynth.isObstacle[X][Y + 1]) {
+			return true;
+		}
+	};
+	if (position.getOrientation == 'S') {
+		if (!labirynth.isObstacle[X][Y - 1]) {
+			return true;
+		}
+	};
+	if (position.getOrientation == 'W') {
+		if (!labirynth.isObstacle[X + 1][Y]) {
+			return true;
+		}
+	};
+	if (position.getOrientation == 'E') {
+		if (!labirynth.isObstacle[X - 1][Y]) {
+			return true;
+		}
+	};
+
+//wybór œciany - póki co na twardo bêdzie to lewa
+
+//trafienie na œciane
+
+
+
+//obs³uga zau³ka
+
+//zawracanie
+
+//ruch
+
+}
